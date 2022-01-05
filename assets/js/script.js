d66bdad873079949937ae384969b2f8d6e04c97d
jaolsen7 <jaolsen@berkeley.edu>
2022-01-04 18:47:22 -0800

    added functional timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
+var timerEl = document.getElementById("timer");
+console.log(timerEl);
+var secondsLeft = 90;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerEl.textContent = "Time Remaining: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      //sendMessage();
+    }
+
+  }, 1000);
+}
+
 var startButton = document.querySelector("#btn");
 
 function startGame () {
     startButton.addEventListener("click", function (event) {
-    alert("button clicked!");
+    setTime();
     })
 };
 startGame();
