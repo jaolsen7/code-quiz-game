90d9d614a90166222a5191a34d796f64ddc8ea93
jaolsen7 <jaolsen@berkeley.edu>
2022-01-04 18:29:08 -0800

    set up all 3 files and got button to click

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var startButton = document.querySelector("#btn");
+
+function startGame () {
+    startButton.addEventListener("click", function (event) {
+    alert("button clicked!");
+    })
+};
+startGame();
